90195c85e41704371b3a8e478faf74b947d913ce
galatea <157182004+gggalatea@users.noreply.github.com>
2024-08-09 09:14:15 +1200

    Add operate function

--- script.js
+++ script.js
@@ -19,3 +19,16 @@ function divide(num1, num2) {
 let num1;
 let operator;
 let num2;
+
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "×":
+            return multiply(num1, num2);
+        case "÷":
+            return divide(num1, num2);
+    }
+}
